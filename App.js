4d052f7ed2b94d3ded1340d093260ef7ffae5048
AaronB <aaronbesson@gmail.com>
2022-07-24 01:38:01 -0400

    moon dance

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { BlurView } from "expo-blur";
 import React, { useState } from "react";
 import {
   Dimensions,
+  Image,
   ImageBackground,
   Modal,
   Pressable,
@@ -10,20 +10,99 @@ import {
   useColorScheme,
   View,
 } from "react-native";
-import Animated, { FlipInXUp, FlipOutXDown } from "react-native-reanimated";
+import Animated, {
+  FlipInXUp,
+  FlipOutXDown,
+  useAnimatedSensor,
+  useAnimatedStyle,
+  SensorType,
+  FlipInXDown,
+  SlideInUp,
+  SlideInDown,
+  ZoomInLeft,
+  ZoomInEasyDown,
+} from "react-native-reanimated";
 import { LinearGradient } from "expo-linear-gradient";
+import { BlurView } from "expo-blur";
+
 
 const { width, height } = Dimensions.get("window");
 
 export default SendModal = ({}) => {
   const colorScheme = useColorScheme();
-  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(true);
+
+  const animatedSensor = useAnimatedSensor(SensorType.ROTATION, {
+    interval: 100,
+  });
+
+  const style = useAnimatedStyle(() => {
+    const { pitch, yaw } = animatedSensor.sensor.value;
+    let yawValue =
+      64 * (yaw < 0 ? 2.5 * Number(yaw.toFixed(2)) : Number(yaw.toFixed(2)));
+    let pitchValue = 36 * pitch.toFixed(2);
+    return {
+      transform: [
+        {
+          translateX: pitchValue,
+        },
+        {
+          translateY: yawValue,
+        },
+        {
+          rotate: (pitchValue / 2 + 'deg')
+        },
+        {
+          skewX: ('4deg')
+        }
+      ],
+    };
+  });
+
+
+  const rocketStyle = useAnimatedStyle(() => {
+    const { pitch, yaw } = animatedSensor.sensor.value;
+    let yawValue =
+      54 * (yaw < 0 ? 2.5 * Number(yaw.toFixed(2)) : Number(yaw.toFixed(2)));
+    let pitchValue = 120 * pitch.toFixed(2);
+    return {
+      transform: [
+        {
+          translateX: pitchValue / 3,
+        },
+        {
+          translateY: yawValue / 3,
+        },
+        {
+          rotate: (pitchValue / 10 + 'deg')
+        }
+      ],
+    };
+  });
+
+  const earth = useAnimatedStyle(() => {
+    const { pitch, yaw } = animatedSensor.sensor.value;
+    let yawValue =
+      120 * (yaw < 0 ? 2.5 * Number(yaw.toFixed(2)) : Number(yaw.toFixed(2)));
+    let pitchValue = 12 * pitch.toFixed(2);
+    return {
+      transform: [
+        {
+          translateX: pitchValue,
+        },
+        {
+          translateY: - yawValue  / 1.5,
+        }
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
       <ImageBackground
         source={
           colorScheme === "light"
-            ? require("./assets/bg1.png")
+            ? require("./assets/bg6.png")
             : require("./assets/bg8.png")
         }
         resizeMode="cover"
@@ -57,24 +136,81 @@ export default SendModal = ({}) => {
               {
                 backgroundColor:
                   colorScheme === "light"
-                    ? "rgba(255,255,255,0)"
+                    ? "rgba(0,0,0,0.85)"
                     : "rgba(0,0,0,0.65)",
               },
             ]}
           >
-            <Animated.View
-              entering={FlipInXUp.duration(500)}
-              exiting={FlipOutXDown.duration(500)}
-              style={[
-                styles.modalView,
-                {
-                  backgroundColor:
-                    colorScheme === "light" ? "#FDB13F" : "#751F98",
-                },
-              ]}
-            >
+            <Animated.View style={style}>
+              <Animated.Image
+                entering={FlipInXDown.duration(500).delay(1500)}
+                exiting={FlipOutXDown.duration(500)}
+                source={require("./assets/spaceman.png")}
+                style={{
+                  width: height / 1.5,
+                  height: height / 1.5,
+                  shadowColor: "plum",
+                  shadowOffset: { width: 0, height: 0 },
+                  shadowRadius: 30,
+                  shadowOpacity: 0.8,
+                }}
+                resizeMode="contain"
+              />
+
               {/* // Add content here */}
             </Animated.View>
+            <Animated.Image
+              entering={ZoomInEasyDown.duration(400)}
+              source={require("./assets/rocket.png")}
+              style={[rocketStyle, {
+                position: "absolute",
+                zIndex: -1,
+                top: width / 1.5,
+                left: 12 ,
+                width: width / 3,
+                height: width / 3,
+                shadowColor: "plum",
+                shadowOffset: { width: 0, height: 0 },
+                shadowRadius: 10,
+                shadowOpacity: 0.6,
+              }]}
+              resizeMode="contain"
+            />
+            <Animated.Image
+              entering={SlideInDown.duration(1500)}
+              source={require("./assets/earth.png")}
+              style={[earth,{
+                position: "absolute",
+                top: width,
+                left: - width / 2,
+                zIndex: -1,
+                width: height,
+                height: height * 1.4,
+                margin: 40,
+                shadowColor: "blue",
+                shadowOffset: { width: 0, height: 0 },
+                shadowRadius: 100,
+                shadowOpacity: 0.8,
+              }]}
+              resizeMode="contain"
+            />
+            <Animated.Image
+              entering={ZoomInEasyDown.duration(400)}
+              source={require("./assets/moon.png")}
+              style={{
+                position: "absolute",
+                zIndex: -1,
+                width: width,
+                height: width,
+                top: - width / 2.5,
+                right: - width / 2.5,
+                shadowColor: "white",
+                shadowOffset: { width: 0, height: 0 },
+                shadowRadius: 30,
+                shadowOpacity: 0.6,
+              }}
+              resizeMode="contain"
+            />
           </View>
           <Pressable
             style={[
